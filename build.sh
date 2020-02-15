efe2929821cc2ca40b58f2b6660c57b2027b1846
alexey.lysiuk <alexey.lysiuk@gmail.com>
2020-02-15 13:31:48 +0200

    update build script with zmusic support

--- build.sh
+++ build.sh
@@ -18,41 +18,36 @@ if [ ! -e build ]; then
 fi
 
 OPENAL_DIR=${DEPS_DIR}openal/
-MPG123_DIR=${DEPS_DIR}mpg123/
-SNDFILE_DIR=${DEPS_DIR}sndfile/
-FSYNTH_DIR=${DEPS_DIR}fluidsynth/
-FSYNTH_LIB_PREFIX=${FSYNTH_DIR}lib/lib
-FSYNTH_LIBS=${FSYNTH_LIB_PREFIX}fluidsynth.a\;${FSYNTH_LIB_PREFIX}glib-2.0.a\;${FSYNTH_LIB_PREFIX}intl.a
+ZMUSIC_DIR=${DEPS_DIR}zmusic/
+OPENAL_DIR=${DEPS_DIR}openal/
 JPEG_DIR=${DEPS_DIR}jpeg/
-OTHER_LIBS=-liconv\ -L${DEPS_DIR}ogg/lib\ -logg\ -L${DEPS_DIR}vorbis/lib\ -lvorbis\ -lvorbisenc\ -L${DEPS_DIR}flac/lib\ -lFLAC
-FRAMEWORKS=-framework\ AudioUnit\ -framework\ AudioToolbox\ -framework\ CoreAudio\ -framework\ CoreMIDI
-LINKER_FLAGS=${OTHER_LIBS}\ ${FRAMEWORKS}
+SDL_DIR=${DEPS_DIR}sdl/
+FLUIDSYNTH_LIBS=${DEPS_DIR}fluidsynth/lib/libfluidsynth.a\ ${DEPS_DIR}fluidsynth/lib/libglib-2.0.a\ ${DEPS_DIR}fluidsynth/lib/libintl.a
+SNDFILE_LIBS=${DEPS_DIR}ogg/lib/libogg.a\ ${DEPS_DIR}vorbis/lib/libvorbis.a\ ${DEPS_DIR}vorbis/lib/libvorbisenc.a\ ${DEPS_DIR}flac/lib/libFLAC.a\ ${DEPS_DIR}sndfile/lib/libsndfile.a
+EXTRA_LIBS=-liconv\ ${DEPS_DIR}mpg123/lib/libmpg123.a\ ${FLUIDSYNTH_LIBS}\ ${SNDFILE_LIBS}
+FRAMEWORKS=-framework\ AudioUnit\ -framework\ AudioToolbox\ -framework\ Carbon\ -framework\ CoreAudio\ -framework\ CoreMIDI\ -framework\ CoreVideo\ -framework\ ForceFeedback
+LINKER_FLAGS=${EXTRA_LIBS}\ ${FRAMEWORKS}
 
 cd build
 export PATH=$PATH:/Applications/CMake.app/Contents/bin
 
-cmake                                                    \
-	-DCMAKE_BUILD_TYPE="Release"                         \
-	-DCMAKE_OSX_DEPLOYMENT_TARGET="10.9"                 \
-	-DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}"           \
-	-DDYN_OPENAL=NO                                      \
-	-DDYN_MPG123=NO                                      \
-	-DDYN_SNDFILE=NO                                     \
-	-DDYN_FLUIDSYNTH=NO                                  \
-	-DFORCE_INTERNAL_ZLIB=YES                            \
-	-DFORCE_INTERNAL_BZIP2=YES                           \
-	-DFORCE_INTERNAL_GME=YES                             \
-	-DPK3_QUIET_ZIPDIR=YES                               \
-	-DOPENAL_INCLUDE_DIR="${OPENAL_DIR}include"          \
-	-DOPENAL_LIBRARY="${OPENAL_DIR}lib/libopenal.a"      \
-	-DMPG123_INCLUDE_DIR="${MPG123_DIR}include"          \
-	-DMPG123_LIBRARIES="${MPG123_DIR}lib/libmpg123.a"    \
-	-DSNDFILE_INCLUDE_DIR="${SNDFILE_DIR}include"        \
-	-DSNDFILE_LIBRARY="${SNDFILE_DIR}lib/libsndfile.a"   \
-	-DFLUIDSYNTH_INCLUDE_DIR="${FSYNTH_DIR}include"      \
-	-DFLUIDSYNTH_LIBRARIES="${FSYNTH_LIBS}"              \
-	-DJPEG_INCLUDE_DIR="${JPEG_DIR}include"              \
-	-DJPEG_LIBRARY="${JPEG_DIR}lib/libjpeg.a"            \
+cmake                                                 \
+	-DCMAKE_BUILD_TYPE="Release"                      \
+	-DCMAKE_OSX_DEPLOYMENT_TARGET="10.9"              \
+	-DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}"        \
+	-DDYN_OPENAL=NO                                   \
+	-DDYN_MPG123=NO                                   \
+	-DDYN_SNDFILE=NO                                  \
+	-DDYN_FLUIDSYNTH=NO                               \
+	-DFORCE_INTERNAL_ZLIB=YES                         \
+	-DFORCE_INTERNAL_BZIP2=YES                        \
+	-DPK3_QUIET_ZIPDIR=YES                            \
+	-DOPENAL_INCLUDE_DIR="${OPENAL_DIR}include"       \
+	-DOPENAL_LIBRARY="${OPENAL_DIR}lib/libopenal.a"   \
+	-DZMUSIC_INCLUDE_DIR="${ZMUSIC_DIR}include"       \
+	-DZMUSIC_LIBRARIES="${ZMUSIC_DIR}lib/libzmusic.a" \
+	-DJPEG_INCLUDE_DIR="${JPEG_DIR}include"           \
+	-DJPEG_LIBRARY="${JPEG_DIR}lib/libjpeg.a"         \
 	..
 make -j2
 
