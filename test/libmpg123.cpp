cba6728672815b32ce4d37e3fdc16b49ddc99c56
alexey.lysiuk <alexey.lysiuk@gmail.com>
2020-12-26 11:39:44 +0200

    add mpg123 test

---
+++ test/libmpg123.cpp
@@ -0,0 +1,11 @@
+#include <mpg123.h>
+
+int main(int argc, char **argv)
+{
+	mpg123_init();
+	mpg123_handle* mh = mpg123_new(NULL, NULL);
+	mpg123_param(mh, MPG123_VERBOSE, 1, 0.);
+	mpg123_delete(mh);
+	mpg123_exit();
+	return 0;
+}
