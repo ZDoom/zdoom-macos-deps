38a0a398e165095fee75dbed3c387b5afeea14df
alexey.lysiuk <alexey.lysiuk@gmail.com>
2021-02-11 15:52:33 +0200

    update sdl2 platform headers

--- deps/sdl2/include/SDL2/SDL_config.h
+++ deps/sdl2/include/SDL2/SDL_config.h
@@ -2,9 +2,9 @@
 #pragma once
 
 #if defined(__x86_64__)
-#   include "x86_64/SDL_config.h"
+#   include "_aedi_x86_64_SDL_config.h"
 #elif defined(__aarch64__)
-#   include "arm64/SDL_config.h"
+#   include "_aedi_arm64_SDL_config.h"
 #else
 #   error Unknown architecture
 #endif
