48651da7ad8787ba815a34bea247bf94f4edc70e
alexey.lysiuk <alexey.lysiuk@gmail.com>
2021-03-30 15:14:44 +0300

    add sdl2 test

---
+++ test/sdl2.cpp
@@ -0,0 +1,19 @@
+#include <SDL.h>
+
+int main()
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		return 1;
+	}
+
+	SDL_Event dummy;
+
+	while (SDL_PollEvent(&dummy))
+	{
+	}
+
+	SDL_Quit();
+
+	return 0;
+}
