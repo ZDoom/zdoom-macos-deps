a96be34437eb66649a1b5b169b8ee2b774573dbb
alexey.lysiuk <alexey.lysiuk@gmail.com>
2021-05-16 10:05:31 +0300

    check return values in mpg123 test

--- test/libmpg123.cpp
+++ test/libmpg123.cpp
@@ -2,10 +2,25 @@
 
 int main(int argc, char **argv)
 {
-    mpg123_init();
+    if (mpg123_init() != MPG123_OK)
+    {
+        return 1;
+    }
+
     mpg123_handle* mh = mpg123_new(NULL, NULL);
-    mpg123_param(mh, MPG123_VERBOSE, 1, 0.);
+
+    if (mh == nullptr)
+    {
+        return 1;
+    }
+
+    if (mpg123_param(mh, MPG123_VERBOSE, 1, 0.) != MPG123_OK)
+    {
+        return 1;
+    }
+
     mpg123_delete(mh);
     mpg123_exit();
+
     return 0;
 }
