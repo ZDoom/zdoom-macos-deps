2a21f052f0f1e4bdc8321334269f771a27c188c1
alexey.lysiuk <alexey.lysiuk@gmail.com>
2021-06-20 14:44:40 +0300

    aedi: remove obsolete jpeg-turbo .pc file processing

--- aedi/target/library_tier1.py
+++ aedi/target/library_tier1.py
@@ -269,17 +269,6 @@ class JpegTurboTarget(CMakeStaticDependencyTarget):
     def detect(self, state: BuildState) -> bool:
         return os.path.exists(state.source + 'turbojpeg.h')
 
-    @staticmethod
-    def _process_pkg_config(pcfile: str, line: str) -> str:
-        if line.startswith('exec_prefix='):
-            return 'exec_prefix=${prefix}\n'
-        elif line.startswith('libdir='):
-            return 'libdir=${exec_prefix}/lib\n'
-        elif line.startswith('includedir='):
-            return 'includedir=${prefix}/include\n'
-
-        return line
-
 
 class MoltenVKTarget(MakeTarget):
     def __init__(self, name='moltenvk'):
