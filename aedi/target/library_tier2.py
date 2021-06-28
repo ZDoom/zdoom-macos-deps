d5fe8eda9ac7da357ecfd12ea37f5621a593ebcd
alexey.lysiuk <alexey.lysiuk@gmail.com>
2021-06-28 10:52:48 +0300

    aedi: update wxwidgets to 3.1.5

--- aedi/target/library_tier2.py
+++ aedi/target/library_tier2.py
@@ -608,30 +608,70 @@ class WebpTarget(CMakeStaticDependencyTarget):
         self.keep_module_target(state, 'WebP::webp')
 
 
-class WxWidgetsTarget(ConfigureMakeStaticDependencyTarget):
+class WxWidgetsTarget(CMakeStaticDependencyTarget):
     def __init__(self, name='wxwidgets'):
         super().__init__(name)
 
+        self.os_version['x86_64'] = StrictVersion('10.10')
+        self.sdk_version['x86_64'] = StrictVersion('10.11')
+
+        opts = self.options
+        opts['wxBUILD_SHARED'] = 'NO'
+        opts['wxUSE_LIBLZMA'] = 'YES'
+        opts['wxUSE_LIBSDL'] = 'NO'
+        opts['wxUSE_LIBJPEG'] = 'sys'
+        opts['wxUSE_LIBPNG'] = 'sys'
+        opts['wxUSE_LIBTIFF'] = 'sys'
+
     def prepare_source(self, state: BuildState):
         state.download_source(
-            'https://github.com/wxWidgets/wxWidgets/releases/download/v3.0.5.1/wxWidgets-3.0.5.1.tar.bz2',
-            '440f6e73cf5afb2cbf9af10cec8da6cdd3d3998d527598a53db87099524ac807',
-            patches='wxwidgets-fix-configure')
-
-    def configure(self, state: BuildState):
-        opts = self.options
-        opts['--disable-shared'] = None
-        opts['--with-macosx-sdk'] = state.sdk_path()
-        opts['--with-macosx-version-min'] = state.os_version()
-        opts['--with-libpng'] = 'sys'
-        opts['--with-libtiff'] = 'sys'
-        opts['--without-sdl'] = None
-        opts['--without-subdirs'] = None
-
-        super().configure(state)
+            'https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.5/wxWidgets-3.1.5.tar.bz2',
+            'd7b3666de33aa5c10ea41bb9405c40326e1aeb74ee725bb88f90f1d50270a224',
+            patches='wxwidgets-library-suffix')
 
     def detect(self, state: BuildState) -> bool:
         return os.path.exists(state.source + 'wx-config.in')
+
+    def post_build(self, state: BuildState):
+        super().post_build(state)
+
+        # Replace prefix in setup.h
+        def patch_setup_h(line: str):
+            prefix = '#define wxINSTALL_PREFIX '
+            return f'{prefix}"/usr/local"\n' if line.startswith(prefix) else line
+
+        setup_h_path = state.install_path + 'lib/wx/include/osx_cocoa-unicode-static-3.1/wx/setup.h'
+        self.update_text_file(setup_h_path, patch_setup_h)
+
+        # Fix a few wx-config entries
+        def patch_wx_config(line: str):
+            prefix = 'prefix=${input_option_prefix-${this_prefix:-'
+            is_cross_func = 'is_cross() '
+            is_cross_test = 'is_cross && target='
+            output_option_cc = '[ -z "$output_option_cc" '
+            output_option_cxx = '[ -z "$output_option_cxx" '
+            output_option_ld = '[ -z "$output_option_ld" '
+            ldlibs_gl = 'ldlibs_gl='
+
+            if line.startswith(prefix):
+                return prefix + '$(cd "${0%/*}/.."; pwd)}}\n'
+            elif line.startswith(is_cross_func):
+                return is_cross_func + '{ false; }\n'
+            elif line.startswith(is_cross_test):
+                return is_cross_test + '""\n'
+            elif line.startswith(output_option_cc):
+                return output_option_cc + '] || echo "gcc"\n'
+            elif line.startswith(output_option_cxx):
+                return output_option_cxx + '] || echo "g++"\n'
+            elif line.startswith(output_option_ld):
+                return output_option_ld + '] || echo "g++ -o"\n'
+            elif line.startswith(ldlibs_gl):
+                return ldlibs_gl + '"-lwx_baseu-3.1 -lwx_osx_cocoau_core-3.1 -framework OpenGL"\n'
+
+            return line
+
+        wx_config_path = state.install_path + 'bin/wx-config'
+        self.update_text_file(wx_config_path, patch_wx_config)
 
 
 class ZstdTarget(CMakeStaticDependencyTarget):
