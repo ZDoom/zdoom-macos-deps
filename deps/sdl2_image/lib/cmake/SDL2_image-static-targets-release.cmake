704db1efc2c799aeed97059f3fcca81d8296944d
alexey.lysiuk <alexey.lysiuk@gmail.com>
2022-07-09 16:34:19 +0300

    update sdl2_image to 2.6.0

---
+++ deps/sdl2_image/lib/cmake/SDL2_image-static-targets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "SDL2_image::SDL2_image-static" for configuration "Release"
+set_property(TARGET SDL2_image::SDL2_image-static APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(SDL2_image::SDL2_image-static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libSDL2_image.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS SDL2_image::SDL2_image-static )
+list(APPEND _IMPORT_CHECK_FILES_FOR_SDL2_image::SDL2_image-static "${_IMPORT_PREFIX}/lib/libSDL2_image.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
