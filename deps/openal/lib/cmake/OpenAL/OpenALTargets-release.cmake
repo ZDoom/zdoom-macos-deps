7d0b1bc62f7a559d0f514b0c771d43f8361dd854
alexey.lysiuk <alexey.lysiuk@gmail.com>
2023-02-04 13:38:03 +0200

    deps: update openal to 1.23.0

--- deps/openal/lib/cmake/OpenAL/OpenALTargets-release.cmake
+++ deps/openal/lib/cmake/OpenAL/OpenALTargets-release.cmake
@@ -12,8 +12,8 @@ set_target_properties(OpenAL::OpenAL PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libopenal.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS OpenAL::OpenAL )
-list(APPEND _IMPORT_CHECK_FILES_FOR_OpenAL::OpenAL "${_IMPORT_PREFIX}/lib/libopenal.a" )
+list(APPEND _cmake_import_check_targets OpenAL::OpenAL )
+list(APPEND _cmake_import_check_files_for_OpenAL::OpenAL "${_IMPORT_PREFIX}/lib/libopenal.a" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
