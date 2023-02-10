d3795a7c218dd254dff98401b65cd4a52ea67672
alexey.lysiuk <alexey.lysiuk@gmail.com>
2023-02-10 12:42:59 +0200

    deps: update sdl2_mixer to 2.6.3

--- deps/sdl2_mixer/lib/cmake/SDL2_mixer/SDL2_mixer-static-targets-release.cmake
+++ deps/sdl2_mixer/lib/cmake/SDL2_mixer/SDL2_mixer-static-targets-release.cmake
@@ -12,8 +12,8 @@ set_target_properties(SDL2_mixer::SDL2_mixer-static PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libSDL2_mixer.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS SDL2_mixer::SDL2_mixer-static )
-list(APPEND _IMPORT_CHECK_FILES_FOR_SDL2_mixer::SDL2_mixer-static "${_IMPORT_PREFIX}/lib/libSDL2_mixer.a" )
+list(APPEND _cmake_import_check_targets SDL2_mixer::SDL2_mixer-static )
+list(APPEND _cmake_import_check_files_for_SDL2_mixer::SDL2_mixer-static "${_IMPORT_PREFIX}/lib/libSDL2_mixer.a" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
