e9154d9accee430f4d0706f6d33ce284712fda0a
alexey.lysiuk <alexey.lysiuk@gmail.com>
2023-07-02 11:46:43 +0300

    aedi: update sdl2 to 2.28.1

--- aedi/target/library_tier2.py
+++ aedi/target/library_tier2.py
@@ -269,8 +269,8 @@ class Sdl2Target(base.CMakeStaticDependencyTarget):
 
     def prepare_source(self, state: BuildState):
         state.download_source(
-            'https://github.com/libsdl-org/SDL/releases/download/release-2.28.0/SDL2-2.28.0.tar.gz',
-            'd215ae4541e69d628953711496cd7b0e8b8d5c8d811d5b0f98fdc7fd1422998a',
+            'https://github.com/libsdl-org/SDL/releases/download/release-2.28.1/SDL2-2.28.1.tar.gz',
+            '4977ceba5c0054dbe6c2f114641aced43ce3bf2b41ea64b6a372d6ba129cb15d',
             patches='sdl2-no-gamecontroller+corehaptic')
 
     def configure(self, state: BuildState):
