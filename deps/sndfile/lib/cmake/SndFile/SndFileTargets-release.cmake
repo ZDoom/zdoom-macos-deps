c097734065e808e4f7843b4906d83add1e034b75
alexey.lysiuk <alexey.lysiuk@gmail.com>
2023-08-14 09:46:21 +0300

    deps: update sndfile to 1.2.2

--- deps/sndfile/lib/cmake/SndFile/SndFileTargets-release.cmake
+++ deps/sndfile/lib/cmake/SndFile/SndFileTargets-release.cmake
@@ -12,8 +12,8 @@ set_target_properties(SndFile::sndfile PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libsndfile.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS SndFile::sndfile )
-list(APPEND _IMPORT_CHECK_FILES_FOR_SndFile::sndfile "${_IMPORT_PREFIX}/lib/libsndfile.a" )
+list(APPEND _cmake_import_check_targets SndFile::sndfile )
+list(APPEND _cmake_import_check_files_for_SndFile::sndfile "${_IMPORT_PREFIX}/lib/libsndfile.a" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
