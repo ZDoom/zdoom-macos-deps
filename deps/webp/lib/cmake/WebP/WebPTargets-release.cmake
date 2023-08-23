73ad049e10e89cb548e1dbfdaf5224315834ede7
alexey.lysiuk <alexey.lysiuk@gmail.com>
2023-08-23 12:46:25 +0300

    deps: add webp 1.3.1

---
+++ deps/webp/lib/cmake/WebP/WebPTargets-release.cmake
@@ -0,0 +1,59 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "WebP::sharpyuv" for configuration "Release"
+set_property(TARGET WebP::sharpyuv APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(WebP::sharpyuv PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libsharpyuv.a"
+  )
+
+list(APPEND _cmake_import_check_targets WebP::sharpyuv )
+list(APPEND _cmake_import_check_files_for_WebP::sharpyuv "${_IMPORT_PREFIX}/lib/libsharpyuv.a" )
+
+# Import target "WebP::webpdecoder" for configuration "Release"
+set_property(TARGET WebP::webpdecoder APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(WebP::webpdecoder PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libwebpdecoder.a"
+  )
+
+list(APPEND _cmake_import_check_targets WebP::webpdecoder )
+list(APPEND _cmake_import_check_files_for_WebP::webpdecoder "${_IMPORT_PREFIX}/lib/libwebpdecoder.a" )
+
+# Import target "WebP::webp" for configuration "Release"
+set_property(TARGET WebP::webp APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(WebP::webp PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libwebp.a"
+  )
+
+list(APPEND _cmake_import_check_targets WebP::webp )
+list(APPEND _cmake_import_check_files_for_WebP::webp "${_IMPORT_PREFIX}/lib/libwebp.a" )
+
+# Import target "WebP::webpdemux" for configuration "Release"
+set_property(TARGET WebP::webpdemux APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(WebP::webpdemux PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libwebpdemux.a"
+  )
+
+list(APPEND _cmake_import_check_targets WebP::webpdemux )
+list(APPEND _cmake_import_check_files_for_WebP::webpdemux "${_IMPORT_PREFIX}/lib/libwebpdemux.a" )
+
+# Import target "WebP::libwebpmux" for configuration "Release"
+set_property(TARGET WebP::libwebpmux APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(WebP::libwebpmux PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libwebpmux.a"
+  )
+
+list(APPEND _cmake_import_check_targets WebP::libwebpmux )
+list(APPEND _cmake_import_check_files_for_WebP::libwebpmux "${_IMPORT_PREFIX}/lib/libwebpmux.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
