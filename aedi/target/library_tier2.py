a79b497900ca8ec7e9e7c63c8b1e671a95cdd070
alexey.lysiuk <alexey.lysiuk@gmail.com>
2023-11-03 13:18:37 +0200

    aedi: remove obsolete sdl2 patch
    clang from xcode 15 drops support for macos sdk older than 11.3 to build c++ code, it cannot locate standard library headers anymore
    without ability to use older macos sdks, it doesn't make any sense to disable gamecontroller and corehaptic support

--- aedi/target/library_tier2.py
+++ aedi/target/library_tier2.py
@@ -270,8 +270,7 @@ class Sdl2Target(base.CMakeStaticDependencyTarget):
     def prepare_source(self, state: BuildState):
         state.download_source(
             'https://github.com/libsdl-org/SDL/releases/download/release-2.28.4/SDL2-2.28.4.tar.gz',
-            '888b8c39f36ae2035d023d1b14ab0191eb1d26403c3cf4d4d5ede30e66a4942c',
-            patches='sdl2-no-gamecontroller+corehaptic')
+            '888b8c39f36ae2035d023d1b14ab0191eb1d26403c3cf4d4d5ede30e66a4942c')
 
     def configure(self, state: BuildState):
         opts = state.options
