e7336c4e8cc27c91e399617f8a873869bd3ba99b
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-03-28 16:01:19 +0200

    aedi: patch fluidsynth sf3 support
    detection of sndfile vorbis/mpeg support is a bit broken right now
    * native target doesn't detect it because of missing flac symbols, can be fixes with `-lFLAC` linker option
    * cross-compiled target doesn't detect it even with the given linker option

--- aedi/target/library_tier2.py
+++ aedi/target/library_tier2.py
@@ -55,7 +55,8 @@ class FluidSynthTarget(base.CMakeStaticDependencyTarget):
     def prepare_source(self, state: BuildState):
         state.download_source(
             'https://github.com/FluidSynth/fluidsynth/archive/refs/tags/v2.3.5.tar.gz',
-            'f89e8e983ecfb4a5b4f5d8c2b9157ed18d15ed2e36246fa782f18abaea550e0d')
+            'f89e8e983ecfb4a5b4f5d8c2b9157ed18d15ed2e36246fa782f18abaea550e0d',
+            patches='fluidsynth-sf3-support')
 
     def configure(self, state: BuildState):
         opts = state.options
