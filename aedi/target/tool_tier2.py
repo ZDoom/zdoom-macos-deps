fd9191e94b05f7f4e4fc691e8906c080f732a2b5
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-04-14 09:46:48 +0300

    aedi: update rizin to 0.7.3

--- aedi/target/tool_tier2.py
+++ aedi/target/tool_tier2.py
@@ -224,8 +224,8 @@ class RizinTarget(base.MesonTarget):
 
     def prepare_source(self, state: BuildState):
         state.download_source(
-            'https://github.com/rizinorg/rizin/releases/download/v0.7.2/rizin-src-v0.7.2.tar.xz',
-            'fcff3fb45ae2b75e3f604bc7a08076e322e6e14def79098186378065ccb3582a')
+            'https://github.com/rizinorg/rizin/releases/download/v0.7.3/rizin-src-v0.7.3.tar.xz',
+            'e0ed25ada6be42098d38da9ccef4befbd549e477e80f8dffa5ca1b8ff9fbda74')
 
     def detect(self, state: BuildState) -> bool:
         return state.has_source_file('binrz/man/rizin.1')
