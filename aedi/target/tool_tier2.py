d195aa2b84d2cea50839a005bd95d9f25300a08d
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-10-20 13:48:54 +0300

    aedi: update radare2 to 5.9.6

--- aedi/target/tool_tier2.py
+++ aedi/target/tool_tier2.py
@@ -206,8 +206,8 @@ class Radare2Target(base.MesonTarget):
 
     def prepare_source(self, state: BuildState):
         state.download_source(
-            'https://github.com/radareorg/radare2/releases/download/5.9.4/radare2-5.9.4.tar.xz',
-            'edf4fc9255482ef790a85e1e563ecce147c6dc6ef49572586b3bb7fa6f1331b3')
+            'https://github.com/radareorg/radare2/releases/download/5.9.6/radare2-5.9.6.tar.xz',
+            '00fd262c8b23d0ce1b733b9dd9b0e7913062dcee0b5cf7c0d3682c041cc1a78d')
 
     def detect(self, state: BuildState) -> bool:
         return state.has_source_file('man/radare2.1')
