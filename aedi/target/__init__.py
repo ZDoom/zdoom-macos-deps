a8d97612be1b226aadc9191eba5d190f00e90a84
alexey.lysiuk <alexey.lysiuk@gmail.com>
2025-01-10 10:01:46 +0200

    aedi: update copyright years

--- aedi/target/__init__.py
+++ aedi/target/__init__.py
@@ -1,6 +1,6 @@
 #
 #    Helper module to build macOS version of various source ports
-#    Copyright (C) 2020-2024 Alexey Lysiuk
+#    Copyright (C) 2020-2025 Alexey Lysiuk
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
