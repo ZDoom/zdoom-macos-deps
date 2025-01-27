13b85bfb873120ce0c61091b8f2d235bce2f97eb
alexey.lysiuk <alexey.lysiuk@gmail.com>
2025-01-27 09:54:06 +0200

    deps: update fluidsynth to 2.4.3

--- deps/fluidsynth/lib/cmake/fluidsynth/FluidSynthConfigVersion.cmake
+++ deps/fluidsynth/lib/cmake/fluidsynth/FluidSynthConfigVersion.cmake
@@ -10,13 +10,13 @@
 # The variable CVF_VERSION must be set before calling configure_file().
 
 
-set(PACKAGE_VERSION "2.4.2")
+set(PACKAGE_VERSION "2.4.3")
 
 if(PACKAGE_VERSION VERSION_LESS PACKAGE_FIND_VERSION)
   set(PACKAGE_VERSION_COMPATIBLE FALSE)
 else()
 
-  if("2.4.2" MATCHES "^([0-9]+)\\.([0-9]+)")
+  if("2.4.3" MATCHES "^([0-9]+)\\.([0-9]+)")
     set(CVF_VERSION_MAJOR "${CMAKE_MATCH_1}")
     set(CVF_VERSION_MINOR "${CMAKE_MATCH_2}")
 
@@ -27,7 +27,7 @@ else()
       string(REGEX REPLACE "^0+" "" CVF_VERSION_MINOR "${CVF_VERSION_MINOR}")
     endif()
   else()
-    set(CVF_VERSION_MAJOR "2.4.2")
+    set(CVF_VERSION_MAJOR "2.4.3")
     set(CVF_VERSION_MINOR "")
   endif()
 
@@ -71,11 +71,6 @@ else()
   endif()
 endif()
 
-
-# if the installed project requested no architecture check, don't perform the check
-if("FALSE")
-  return()
-endif()
 
 # if the installed or the using project don't have CMAKE_SIZEOF_VOID_P set, ignore it:
 if("${CMAKE_SIZEOF_VOID_P}" STREQUAL "" OR "8" STREQUAL "")
