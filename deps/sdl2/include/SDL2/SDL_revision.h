50186a4812e3b0421f3878722ac6ebb61eaca084
alexey.lysiuk <alexey.lysiuk@gmail.com>
2025-03-02 10:19:46 +0200

    deps: update sdl2 to 2.32.2

--- deps/sdl2/include/SDL2/SDL_revision.h
+++ deps/sdl2/include/SDL2/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-release-2.32.0-0-g7a44b1ab0 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-release-2.32.2-0-ge11183ea6 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-release-2.32.0-0-g7a44b1ab0"
+#define SDL_REVISION "SDL-release-2.32.2-0-ge11183ea6"
 #endif
