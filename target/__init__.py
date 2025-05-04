4be2f093fe7a216602a60a24a7b2cc9b0786d4a3
alexey.lysiuk <alexey.lysiuk@gmail.com>
2025-05-04 10:16:19 +0300

    aedi: remove non-zdoom targets

--- target/__init__.py
+++ target/__init__.py
@@ -16,11 +16,8 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from .library_tier1 import *
-from .library_tier2 import *
-from .library_tier3 import *
+from .library import *
 from .main import *
-from .tool import *
 
 
 def targets():
@@ -37,21 +34,8 @@ def targets():
         WadExtTarget(),
         ZdbspTarget(),
         ZDRayTarget(),
-        PrBoomPlusTarget(),
-        DsdaDoom(),
-        ChocolateDoomTarget(),
-        CrispyDoomTarget(),
-        RudeTarget(),
-        WoofTarget(),
-        DoomRetroTarget(),
-        Doom64EXTarget(),
-        DevilutionXTarget(),
-        EDuke32Target(),
-        NBloodTarget(),
-        QuakespasmTarget(),
-        QuakespasmExpTarget(),
 
-        # Libraries needed for GZDoom and Raze
+        # Libraries
         Bzip2Target(),
         FfiTarget(),
         FlacTarget(),
@@ -70,73 +54,4 @@ def targets():
         VorbisTarget(),
         VpxTarget(),
         ZMusicTarget(),
-
-        # Libraries needed for other targets
-        DumbTarget(),
-        FluidSynthTarget(),
-        FmtTarget(),
-        GmeTarget(),
-        InstPatchTarget(),
-        MadTarget(),
-        MikmodTarget(),
-        ModPlugTarget(),
-        OpusFileTarget(),
-        PngTarget(),
-        PortMidiTarget(),
-        SamplerateTarget(),
-        Sdl2Target(),
-        Sdl2ImageTarget(),
-        Sdl2MixerTarget(),
-        Sdl2NetTarget(),
-        SodiumTarget(),
-        VulkanHeadersTarget(),
-        VulkanLoaderTarget(),
-        WavPackTarget(),
-        WebpTarget(),
-        XmpTarget(),
-        ZlibNgTarget(),
-
-        # Obsolete libraries without binaries
-        BrotliTarget(),
-        ExpatTarget(),
-        FftwTarget(),
-        FobosTarget(),
-        FreeImageTarget(),
-        FreeTypeTarget(),
-        FtglTarget(),
-        GlewTarget(),
-        GlfwTarget(),
-        HarfBuzzTarget(),
-        HighwayTarget(),
-        JpegTurboTarget(),
-        LuaTarget(),
-        Sdl2TtfTarget(),
-        SfmlTarget(),
-        TiffTarget(),
-        UsbTarget(),
-        WxWidgetsTarget(),
-        ZstdTarget(),
-
-        # Tools without binaries stored in the repo, can be outdated
-        AutoconfTarget(),
-        AutomakeTarget(),
-        Bzip3Target(),
-        DfuUtilTarget(),
-        DosBoxXTarget(),
-        DzipTarget(),
-        EricWToolsTarget(),
-        FFmpegTarget(),
-        GlslangTarget(),
-        HackRFTarget(),
-        M4Target(),
-        P7ZipTarget(),
-        PbzxTarget(),
-        QPakManTarget(),
-        Radare2Target(),
-        RizinTarget(),
-        SeverZipTarget(),
-        UnrarTarget(),
-        XdeltaTarget(),
-        XzTarget(),
-        ZipTarget(),
     )
