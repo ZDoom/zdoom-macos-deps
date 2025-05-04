bf3230abdc586340369d117b52033722d9399359
alexey.lysiuk <alexey.lysiuk@gmail.com>
2025-05-04 10:12:38 +0300

    deps: remove non-zdoom dependencies

--- deps/sdl2/include/SDL2/SDL_revision.h
+++
@@ -1,8 +0,0 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-release-2.32.4-0-g2359383fc (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-release-2.32.4-0-g2359383fc"
-#endif
