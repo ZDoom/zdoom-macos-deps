333af53dd1ea6031208ffdff545055d6833d0b6f
alexey.lysiuk <alexey.lysiuk@gmail.com>
2025-06-13 17:21:08 +0300

    aedi: update mpg123 to 1.33.0

--- target/library.py
+++ target/library.py
@@ -303,8 +303,8 @@ class Mpg123Target(base.CMakeStaticDependencyTarget):
 
     def prepare_source(self, state: BuildState):
         state.download_source(
-            'https://www.mpg123.de/download/mpg123-1.32.10.tar.bz2',
-            '87b2c17fe0c979d3ef38eeceff6362b35b28ac8589fbf1854b5be75c9ab6557c',
+            'https://www.mpg123.de/download/mpg123-1.33.0.tar.bz2',
+            '2290e3aede6f4d163e1a17452165af33caad4b5f0948f99429cfa2d8385faa9d',
             patches=('mpg123-have-fpu', 'mpg123-no-syn123'))
 
     def configure(self, state: BuildState):
